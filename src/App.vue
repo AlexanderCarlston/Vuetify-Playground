7d4ac7a0d0e3b9ab17340d46c8051c876342f0ac
alekscarlston@yahoo.com <alekscarlston@yahoo.com>
2018-07-18 16:36:16 -0600

    init commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <v-app>
+    <VuetifyComponent
+    :intro="intro"
+    />
+  </v-app>
+</template>
+
+<script>
+import VuetifyComponent from './components/VuetifyComponent'
+
+export default {
+  name: 'App',
+  components: {
+    VuetifyComponent
+  },
+  data(){
+    return {
+      intro: {
+        title: "Welcome to my Vuetify Playground!",
+        purpose: "The purpose of this is to develop responsive, reusable, and custmoizable components."
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/VuetifyComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container>
+  <h1>{{Intro.title}}</h1>
+  <p>{{Intro.purpose}}</p>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: 'VuetifyComponent',
+  props: ["Intro"]
+}
+</script>
+
+<style scoped>
+
+</style>
